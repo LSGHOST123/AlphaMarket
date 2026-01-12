506fa0117a80d90bb4f6bba5fa87d293d5e0b2a6
Lucas Simioni <familiasimionischiavan1@gmail.com>
2026-01-12 16:08:44 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/AlphaMarket/", // <--- ESTA LINHA É A SOLUÇÃO DO PROBLEMA
   server: {
     port: 3000,
   },
